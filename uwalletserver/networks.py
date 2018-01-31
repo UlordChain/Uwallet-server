ef2821864f3946aa76527940e8f569cb5f894abe
qupengcheng <qupengcheng4514348@qq.com>
2018-01-31 17:44:06 +0800

    change testnet modify prefix 36 to 130

--- uwalletserver/networks.py
+++ uwalletserver/networks.py
@@ -8,7 +8,7 @@ params = {
     'unet_main': {
         'pubkey_address': 0,
         'script_address': 5,
-        'pubkey_address_prefix': 36,
+        'pubkey_address_prefix': 130,
         'script_address_prefix': 16, #204
         'genesis_hash': '000002287d4bdfb69539d264be0eae5f08c8f990732b84cb6c0834bcee80de3a',
         'default_rpc_port': 9457  #9998
