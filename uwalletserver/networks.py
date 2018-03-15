8e07aefca1d8b8522737894b10dc46da20b90ec0
qupengcheng <qupengcheng4514348@qq.com>
2018-03-15 10:21:15 +0800

    genesis_hash

--- uwalletserver/networks.py
+++ uwalletserver/networks.py
@@ -10,7 +10,7 @@ params = {
         'script_address': 5,
         'pubkey_address_prefix': 130,#68
         'script_address_prefix': 125, #204
-        'genesis_hash': '000009c278dda2285ff7d1595d919b2ae1f3728306409f50e374ea313391db8f',
+        'genesis_hash': '000000e32e974118821c865e0f79cd851edd96ccdf161de997ee85c438d0e7e3',
         'default_rpc_port': 19889  #9998
     },
     'unet_test': {
